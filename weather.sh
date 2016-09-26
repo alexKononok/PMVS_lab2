03134c7476d7fed48c9bcbf6da055edc7b60cab0
alexKononok <alex.kononok@gmail.com>
2016-09-26 15:37:04 +0300

    correction prog

--- weather.sh
+++ weather.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+source config.ini
 
 while [ 1 = 1 ]
 do
-	sleep 5
 	wget -q https://yandex.by/pogoda/minsk/index.html
 	grep -o 'current-weather__thermometer current-weather__thermometer_type_now">[+|-][0-9]\+' index.html | grep -o "[+|-][0-9]\+"
+	sleep $Timeout
 	rm *.html
 done
 
