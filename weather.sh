f6718ca520a4158090790304d9525d5f18b26380
alexKononok <alex.kononok@gmail.com>
2016-09-26 15:15:00 +0300

    true functioning prog

--- weather.sh
+++ weather.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
+while [ 1 = 1 ]
+do
+	sleep 5
 	wget -q https://yandex.by/pogoda/minsk/index.html
-	grep "current-weather__thermometer current-weather__thermometer_type_now" index.html | grep -o "[+|-][0-9]\+"
-	
+	grep -o 'current-weather__thermometer current-weather__thermometer_type_now">[+|-][0-9]\+' index.html | grep -o "[+|-][0-9]\+"
+	rm *.html
+done
+
 
